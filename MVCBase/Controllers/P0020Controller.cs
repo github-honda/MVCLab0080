2623b3856763ef06bba249426bc9af21280a4cca
github-honda <honda@luckstar.com.tw>
2016-10-06 17:33:42 +0800

    First commit

---
+++ MVCBase/Controllers/P0020Controller.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCBase.Models;
+
+namespace MVCBase.Controllers
+{
+    public class P0020Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            // 多筆清單顯示. Controller預設action為Index, 詳於~/App_Start/RouteConfig.cs.
+            return View(new P0020().Index());
+        }
+        public ActionResult Read(string id)
+        {
+            // 單筆顯示
+            return View(new P0020().Read(id));
+        }
+        public ActionResult Create()
+        {
+            // 顯示新增網頁
+            return View();
+        }
+        public ActionResult Update(string id)
+        {
+            // 顯示修改畫面
+            return View(new P0020().Read(id));
+        }
+        public ActionResult TestModelBinder()
+        {
+            // 測試Model Binder
+            return View();
+        }
+        public ActionResult SubmitCreate(T0010 t1, string btnSubmit)
+        {
+            // 執行新增
+            int iAffected = 0;
+            switch (btnSubmit)
+            {
+                case "新增":
+                    iAffected = new P0020().Create(t1);
+                    break;
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult SubmitUpdate(T0010 t1, string btnSubmit)
+        {
+            // 執行修改或刪除
+            int iAffected = 0;
+            switch (btnSubmit)
+            {
+                case "修改":
+                    iAffected = new P0020().Update(t1);
+                    break;
+                case "刪除":
+                    iAffected = new P0020().Delete(t1.ms1);
+                    break;
+            }
+            return RedirectToAction("Index");
+        }
+        public string TestSubmitModelBinder(TestModelBinder e, string ms5, string btnSubmit)
+        {
+            // ModelBinder會自動比對(URL接收欄位)與(action函數參數欄位), 相同名稱的欄位會自動填入接收的資料內容.
+            // (action函數參數欄位)比對範圍為: (函數中的參數名稱)以及(class中的所有property欄位, 包括子class). 
+            // 也可由Request.Form["欄位名稱"]方式取得接收資料.
+            return "ms1=" + e.ms1 + // 比對property名稱符合
+                "<br />, ms2=" + e.ms2 + // 比對property名稱符合
+                "<br />, ms3=" + e.ms3 + // 比對property名稱符合
+                "<br />, ms4=" + e.ms4 + // 比對property名稱符合
+                "<br />, ms5=" + ms5 + // 比對函數參數名稱符合.
+                "<br />, ms6=" + Request.Form["ms6"] + // 自行取得
+                "<br />, mT0010.ms1=" + e.mT0010.ms1 + // 比對子class欄位.
+                "<br />, mT0010.ms2=" + e.mT0010.ms2 + // 比對子class欄位.
+                "<br />, mT0010.mi1=" + e.mT0010.mi1 + // 比對子class欄位.
+                "<br />, mT0010.mi2=" + e.mT0010.mi2 + // 比對子class欄位.
+                "<br />, btnSumit=" + btnSubmit; // 比對函數參數名稱符合.
+        }
+    }
+}
+
+
+
---
+++ MVCBase/Models/P0020.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCBase.ViewModels;
+
+namespace MVCBase.Models
+{
+    public class P0020
+    {
+        public int Create(T0010 t1)
+        {
+            // 新增
+            int iAffected = new T0010().Create(t1);
+            return iAffected;
+        }
+        public int Update(T0010 t1)
+        {
+            // 修改
+            int iAffected = new T0010().Update(t1);
+            return iAffected;
+        }
+        public int Delete(string id)
+        {
+            // 刪除
+            int iAffected = new T0010().Delete(id);
+            return iAffected;
+        }
+
+        public P0010ViewModel Read(string id)
+        {
+            // 單筆顯示
+            P0010ViewModel vm1 = new P0010ViewModel();
+            T0010 t1 = new T0010().Read1Record(id);
+            vm1.ms1 = t1.ms1; // 學號
+            vm1.ms2 = t1.ms2; // 姓名
+            vm1.mi1 = t1.mi1; // 國文分數
+            vm1.mi2 = t1.mi2; // 英文分數
+            vm1.miSum = vm1.mi1 + vm1.mi2; // 計算總分
+            vm1.mi1Extra = vm1.miSum / 2;  // 計算平均分數
+            if ((vm1.mi1Extra) < 60)
+                vm1.msColor = "red"; // 平均低於60分的話, 以紅色顯示
+            else
+                vm1.msColor = "green";
+            return vm1;
+        }
+
+        public P0010ListViewModel Index()
+        {
+            // 多筆清單顯示
+            P0010ListViewModel vm1 = new P0010ListViewModel();
+            List<T0010> listT1 = new T0010().ReadList(); // 來自資料庫的清單
+            List<P0010ViewModel> listBrowse1 = new List<P0010ViewModel>(); // 顯示在View上的清單
+            foreach (T0010 t1 in listT1)
+            {
+                P0010ViewModel row1 = new P0010ViewModel();
+                row1.ms1 = t1.ms1; // 學號
+                row1.ms2 = t1.ms2; // 姓名
+                row1.mi1 = t1.mi1; // 國文分數
+                row1.mi2 = t1.mi2; // 英文分數
+                row1.miSum = row1.mi1 + row1.mi2; // 計算總分
+                row1.mi1Extra = row1.miSum / 2; // 計算平均分數
+                if ((row1.mi1Extra) < 60)
+                    row1.msColor = "red"; // 平均低於60分的話, 以紅色顯示
+                else
+                    row1.msColor = "green";
+                listBrowse1.Add(row1);
+            }
+            vm1.msName = "3年2班";
+            vm1.mList = listBrowse1;
+            return vm1;
+        }
+    }
+}
+
+
